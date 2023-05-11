6b298556077a7968f5794e9188e6d81b64a0d36c
InfamousPlayer <93223147+InfamousPlayer@users.noreply.github.com>
2023-05-11 17:43:05 -0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+import BottomTabNavigator from "./components/BottomTabNavigator";
+
+export default class App extends Components {
+  render(){
+  return <BottomTabNavigator/>;
+}
+}
+
